915d340216eea61a4a4fc49340ffccd0eee9dcc9
MMBKMC <151632121+MMBKMC@users.noreply.github.com>
2024-05-10 17:57:52 +0630

    Update runner.py

--- runner.py
+++ runner.py
@@ -3,6 +3,10 @@ from main import token, string
 
 token1 = "[print],"
 token2 = "[Hello World]"
+token3 = "[print],"
+token4 = "[Good Bye World]"
 
 print(token1)
 print(token2)
+print(token3)
+print(token4)
